ba9f84e5241adadcaa422eb273bb5d1862becb87
UkraineInTheMembrane <jason@ambitionenterprise.ca>
2018-06-27 23:05:23 -0700

    Initial Commit

---
+++ login.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python 
+
+# Define our user login information for their specific gmail account
+ORG_EMAIL   = "@gmail.com"
+FROM_EMAIL  = "your-email" + ORG_EMAIL
+FROM_PWD    = "your-password"
+SMTP_SERVER = "imap.gmail.com"
+SMTP_PORT   = 993
