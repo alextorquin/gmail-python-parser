2fe072753d64357a97bc46848539efdf0b9ae9d9
UkraineInTheMembrane <jason@ambitionenterprise.ca>
2018-06-28 21:34:29 -0700

    Added close and logout

--- fetchEmail.py
+++ fetchEmail.py
@@ -7,6 +7,7 @@
 # A python script designed to consume and parse emails from Gmail
 # and push the data to a MySQL database.
 
+''' import our required modules '''
 import smtplib
 import imaplib
 import email
@@ -20,19 +21,22 @@ def read_email_from_gmail():
         mail.login(cred.FROM_EMAIL, cred.FROM_PWD)
         mail.select('inbox')
 
+        ''' Search through our selected folder '''
         type, data = mail.search(None, 'ALL')
         mail_ids = data[0]
 
+        ''' Count our emails '''
         id_list = mail_ids.split()   
         first_email_id = int(id_list[0])
         latest_email_id = int(id_list[-1])
 
-
+        ''' iterate through each of our counted emails '''
         for i in range(latest_email_id, first_email_id, -1):
             type, data = mail.fetch(i, '(RFC822)' )
-
             for response_part in data:
-                if isinstance(response_part, tuple):
+
+                try:
+                    ''' Load our captured email data into variables '''
                     msg = email.message_from_string(response_part[1])
                     email_subject = msg['subject']
                     email_from = msg['from']
@@ -41,7 +45,15 @@ def read_email_from_gmail():
                     captured_email = func.ParseEmail(email_from, email_subject, msg)
                     return captured_email.find_specific()
 
+                except Exception, e:
+                    pass
+
     except Exception, e:
         print str(e)
+
+    # Close our selected folder and logout 
+    finally:
+        mail.close()
+        mail.logout()
 
 read_email_from_gmail()
